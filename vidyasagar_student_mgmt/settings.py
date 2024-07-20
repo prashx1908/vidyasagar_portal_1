03ce0360103c9724c8ae24eacb1adba2c0b94f22
Prashanth G <77740840+prashx1908@users.noreply.github.com>
2024-07-21 01:02:26 +0530

    Update settings.py

--- vidyasagar_student_mgmt/settings.py
+++ vidyasagar_student_mgmt/settings.py
@@ -48,10 +48,10 @@ TEMPLATES = [
         },
     },
 ]
+
 DATABASES = {
     'default': dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600, ssl_require=True)
 }
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
