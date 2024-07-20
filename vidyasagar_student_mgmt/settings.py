333cc838751129f59791620bbd146dc127a27190
Prashanth G <77740840+prashx1908@users.noreply.github.com>
2024-07-21 00:01:14 +0530

    Update settings.py

--- vidyasagar_student_mgmt/settings.py
+++ vidyasagar_student_mgmt/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*").split(",")
+ALLOWED_HOSTS = ['.vercel.app']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
